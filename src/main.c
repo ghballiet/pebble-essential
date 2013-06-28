ea219ad18c326ab358142ddc5838664594f51ea4
Glen Hunt <glenrhunt@gmail.com>
2013-06-27 18:32:59 -0700

    work!

--- src/main.c
+++ src/main.c
@@ -11,19 +11,53 @@ PBL_APP_INFO(MY_UUID,
 			 APP_INFO_STANDARD_APP);
 
 Window window;
-PblTm t;
+
+TextLayer text_time_layer;
 
 void handle_init(AppContextRef ctx) {
-  (void)ctx;
+	(void)ctx;
 
-  window_init(&window, "Window Name");
-  window_stack_push(&window, true /* Animated */);
+	window_init(&window, "Essential");
+	window_stack_push(&window, true /* Animated */);
+	window_set_background_color(&window, GColorBlack);
+
+	resource_init_current_app(&APP_RESOURCES);
+
+	text_layer_init(&text_time_layer, window.layer.frame);
+	text_layer_set_text_color(&text_time_layer, GColorWhite);
+	text_layer_set_background_color(&text_time_layer, GColorClear);
+	layer_set_frame(&text_time_layer.layer, GRect(7, 92, 144-7, 168-92));
+	text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PRAGMATA_BOLD_48)));
+	layer_add_child(&window.layer, &text_time_layer.layer);
+}
+
+void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
+	(void)ctx;
+
+	// need to be static because they're used by the system later.
+	static char time_text[] = '00:00';
+
+	char *time_format;
+
+	if(clock_is_24h_style())
+		time_format = "%R";
+	else
+		time_format = "%I:%M";
+
+	string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+
+	text_layer_set_text(&text_time_layer, time_text);
 }
 
 
 void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-	.init_handler = &handle_init
-  };
-  app_event_loop(params, &handlers);
+	PebbleAppHandlers handlers = {
+		.init_handler = &handle_init,
+
+		.tick_info = {
+			.tick_handler = &handle_minute_tick,
+			.tick_units = MINUTE_UNIT
+		}
+	};
+	app_event_loop(params, &handlers);
 }
