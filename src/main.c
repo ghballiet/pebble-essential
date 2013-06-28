a27bb0eff22275b61350d07fe7d75f3ba1cf7842
Glen Hunt <glenrhunt@gmail.com>
2013-06-27 20:06:18 -0700

    moving stuff

--- src/main.c
+++ src/main.c
@@ -36,19 +36,19 @@ void handle_init(AppContextRef ctx) {
 
 	resource_init_current_app(&APP_RESOURCES);
 
-	text_layer_init(&text_date_layer, window.layer.frame);
-	text_layer_set_text_color(&text_date_layer, GColorWhite);
-	text_layer_set_background_color(&text_date_layer, GColorClear);
-	layer_set_frame(&text_date_layer.layer, GRect(8, 130, 144-8, 168-130));
-	text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PRAGMATA_24)));
-	layer_add_child(&window.layer, &text_date_layer.layer);
-
 	text_layer_init(&text_time_layer, window.layer.frame);
 	text_layer_set_text_color(&text_time_layer, GColorWhite);
 	text_layer_set_background_color(&text_time_layer, GColorClear);
-	layer_set_frame(&text_time_layer.layer, GRect(7, 57, 144-7, 168-57));
+	layer_set_frame(&text_time_layer.layer, GRect(7, 25, 144-7, 168-25));
 	text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PRAGMATA_BOLD_48)));
 	layer_add_child(&window.layer, &text_time_layer.layer);
+
+	text_layer_init(&text_date_layer, window.layer.frame);
+	text_layer_set_text_color(&text_date_layer, GColorWhite);
+	text_layer_set_background_color(&text_date_layer, GColorClear);
+	layer_set_frame(&text_date_layer.layer, GRect(14, 130, 144-14, 168-130));
+	text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PRAGMATA_24)));
+	layer_add_child(&window.layer, &text_date_layer.layer);
 }
 
 void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
