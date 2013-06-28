05bb084165b044c113dc382fb6beb756bba87496
Glen Hunt <glenrhunt@gmail.com>
2013-06-27 20:08:05 -0700

    testing

--- src/main.c
+++ src/main.c
@@ -39,14 +39,14 @@ void handle_init(AppContextRef ctx) {
 	text_layer_init(&text_time_layer, window.layer.frame);
 	text_layer_set_text_color(&text_time_layer, GColorWhite);
 	text_layer_set_background_color(&text_time_layer, GColorClear);
-	layer_set_frame(&text_time_layer.layer, GRect(7, 25, 144-7, 168-25));
+	layer_set_frame(&text_time_layer.layer, GRect(9, 30, 144-9, 168-30));
 	text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PRAGMATA_BOLD_48)));
 	layer_add_child(&window.layer, &text_time_layer.layer);
 
 	text_layer_init(&text_date_layer, window.layer.frame);
 	text_layer_set_text_color(&text_date_layer, GColorWhite);
 	text_layer_set_background_color(&text_date_layer, GColorClear);
-	layer_set_frame(&text_date_layer.layer, GRect(14, 130, 144-14, 168-130));
+	layer_set_frame(&text_date_layer.layer, GRect(16, 80, 144-16, 168-80));
 	text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PRAGMATA_24)));
 	layer_add_child(&window.layer, &text_date_layer.layer);
 }
